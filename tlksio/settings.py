8cbb2a00ad30aa88cfc55deb3afbf0ec02dcc544
Raül Pérez <repejota@gmail.com>
2017-03-22 22:48:21 -0400

    Added static css and js

--- tlksio/settings.py
+++ tlksio/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/raul/tlksio/'
+STATICFILES_DIRS = [
+    '/home/raul/tlksio/static/',
+]
