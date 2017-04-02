2ce4881dd6aa6156c8fa1b8eea0bd77e0f84e8ab
Raül Pérez <repejota@gmail.com>
2017-04-02 16:44:41 -0400

    Clean unused code

--- talks/admin.py
+++ talks/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
 from .models import Talk
-"""
-from .models import Vote
-from .models import Favorite
-"""
 from .models import Profile
 
 
@@ -15,21 +11,8 @@ admin.site.register(Profile, ProfileAdmin)
 
 
 class TalkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created', 'updated')
+    list_display = ('title', 'type', 'created', 'updated')
     list_filter = ['created', 'type']
     search_fields = ['title']
 
 admin.site.register(Talk, TalkAdmin)
-
-"""
-class VoteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'talk')
-
-admin.site.register(Vote, VoteAdmin)
-
-
-class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'talk')
-
-admin.site.register(Favorite, FavoriteAdmin)
-"""
