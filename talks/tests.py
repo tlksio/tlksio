c67613e2d09b07bcee6fccbd1856f767c59e5dd7
Raül Pérez <repejota@gmail.com>
2017-04-03 08:28:50 -0400

    More tests

--- talks/tests.py
+++ talks/tests.py
@@ -2,6 +2,7 @@ import pytz
 from datetime import datetime
 
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
 from .models import Talk
 
@@ -18,3 +19,21 @@ class TalkTests(TestCase):
 
         talks = Talk.objects.filter(slug="talk-1-title")
         self.assertIs(talks.count(), 1)
+
+
+    def test_create_talk_without_title(self):
+        talk = Talk()
+        talk.code = "talk2code"
+        talk.created = datetime.now(pytz.utc)
+        talk.updated = datetime.now(pytz.utc)
+        with self.assertRaises(IntegrityError):
+            talk.save()
+
+
+    def test_create_talk_without_code(self):
+        talk = Talk()
+        talk.title = "talk 3 title"
+        talk.created = datetime.now(pytz.utc)
+        talk.updated = datetime.now(pytz.utc)
+        with self.assertRaises(IntegrityError):
+            talk.save()
