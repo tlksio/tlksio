fe68c188621fef9a33c4d0d2f93c254207fa55dc
Raül Pérez <repejota@gmail.com>
2017-04-03 08:06:52 -0400

    Talk title and talk code mandatory added tests

--- talks/tests.py
+++ talks/tests.py
@@ -8,10 +8,13 @@ from .models import Talk
 
 class TalkTests(TestCase):
 
-    def test_create_tag(self):
+    def test_create_talk(self):
         talk = Talk()
+        talk.title = "talk 1 title"
+        talk.code = "talk1code"
         talk.created = datetime.now(pytz.utc)
         talk.updated = datetime.now(pytz.utc)
         talk.save()
 
-        self.assertIs(Talk.objects.all().count(), 1)
+        talks = Talk.objects.filter(slug="talk-1-title")
+        self.assertIs(talks.count(), 1)
